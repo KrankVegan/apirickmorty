7a94b7e43f0130fe74eca6b5f7f4d6b3930a20f0
KrankVegan <martinezcawork@gmail.com>
2024-06-12 15:23:28 -0500

    Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharactersComponent } from './characters/characters.component';
+import { LocationsComponent } from './locations/locations.component';
+import { EpisodesComponent } from './episodes/episodes.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent,
+    LocationsComponent,
+    EpisodesComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
